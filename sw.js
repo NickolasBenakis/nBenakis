88472082dc02325bed11635b29df1cb6794ba664
nickolasBenakis <nickolasbenele@gmail.com>
2019-09-21 22:28:24 +0300

    Add sw final stage

--- sw.js
+++ sw.js
@@ -1,47 +1,20 @@
-(() => {
-    const cacheName = 'news-v1';
-    const staticAssets = [
-        './index.html',
-        './index.js',
-        './index.scss',
-        './toggle.scss',
-        './fallback.json',
-        './images/fallback_image.jpg',
-    ];
+var cacheName = 'hello-pwa';
+var filesToCache = ['/', './index.html', './index.scss', './index.js'];
 
-    self.addEventListener('install', async function() {
-        const cache = await caches.open(cacheName);
-        cache.addAll(staticAssets);
-    });
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            return cache.addAll(filesToCache);
+        })
+    );
+});
 
-    self.addEventListener('activate', event => {
-        event.waitUntil(self.clients.claim());
-    });
-
-    self.addEventListener('fetch', event => {
-        const request = event.request;
-        const url = new URL(request.url);
-        if (url.origin === location.origin) {
-            event.respondWith(cacheFirst(request));
-        } else {
-            event.respondWith(networkFirst(request));
-        }
-    });
-
-    async function cacheFirst(request) {
-        const cachedResponse = await caches.match(request);
-        return cachedResponse || fetch(request);
-    }
-
-    async function networkFirst(request) {
-        const dynamicCache = await caches.open('news-dynamic');
-        try {
-            const networkResponse = await fetch(request);
-            dynamicCache.put(request, networkResponse.clone());
-            return networkResponse;
-        } catch (err) {
-            const cachedResponse = await dynamicCache.match(request);
-            return cachedResponse || (await caches.match('./fallback.json'));
-        }
-    }
-})();
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+            return response || fetch(e.request);
+        })
+    );
+});
