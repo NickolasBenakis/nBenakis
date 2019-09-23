69ea8bafacedc39a33f0b84f76855bcc6e323b8e
nickolasBenakis <nickolasbenele@gmail.com>
2019-09-23 20:57:38 +0300

    PWA 100%

--- sw.js
+++ sw.js
@@ -1,20 +1,46 @@
-var cacheName = 'hello-pwa';
-var filesToCache = ['/', './index.html', './index.scss', './index.js'];
+'use strict';
+const staticCacheName = 'pages-cache-v1';
+const filesToCache = [
+    '/',
+    'index.html',
+    'styles/index.scss',
+    'styles/toggle.scss',
+];
 
-/* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function(e) {
-    e.waitUntil(
-        caches.open(cacheName).then(function(cache) {
+self.addEventListener('install', function(event) {
+    console.log('Attempting to install service worker and cache static assets');
+    event.waitUntil(
+        self.skipWaiting(),
+        caches.open(staticCacheName).then(function(cache) {
             return cache.addAll(filesToCache);
         })
     );
 });
 
-/* Serve cached content when offline */
-self.addEventListener('fetch', function(e) {
-    e.respondWith(
-        caches.match(e.request).then(function(response) {
-            return response || fetch(e.request);
-        })
+self.addEventListener('fetch', event => {
+    console.log('Fetch event for ', event.request.url);
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then(response => {
+                if (response) {
+                    console.log('Found ', event.request.url, ' in cache');
+                    return response;
+                }
+                console.log('Network request for ', event.request.url);
+                return fetch(event.request).then(response => {
+                    if (response.status === 404) {
+                        return caches.match('index.html');
+                    }
+                    return caches.open(staticCacheName).then(cache => {
+                        cache.put(event.request.url, response.clone());
+                        return response;
+                    });
+                });
+            })
+            .catch(error => {
+                console.log('Error, ', error);
+                return caches.match('index.html');
+            })
     );
 });
